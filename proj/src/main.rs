7ad6a8a1c03e9aa76f5757bb547cf798f877f863
GamerCleanVic <jonassd2000@gmail.com>
2024-03-10 18:05:37 -0300

    +consts +u32

--- proj/src/main.rs
+++ proj/src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Olá Rust!");
+const SECONDS_IN_MINUTE: u32 = 60;
+
+fn main(){
+    const MINUTES_IN_HOUR: u32 = 60;
+    const SECONDS_IN_HOUR: u32 =
+    SECONDS_IN_MINUTE * MINUTES_IN_HOUR;
+
+    let total = 30;
+    println!("\nTrabalhou {} horas\n", total);
+    {
+        let total = total + 40;
+        println!("Trabalhou {} horas.\n", total);
+    }
+    let total_in_seconds = total + SECONDS_IN_HOUR;
+    println!("Trabalhou {} segundos.\n", total_in_seconds);
+
+    let total: &str = "Quarenta";
+    println!("Trabalhou {} horas.\n", total);
 }
